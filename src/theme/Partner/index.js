267686d3873c11c268cfc1e09a75d402525742be
PulseHarbor <189056730+PulseHarbor@users.noreply.github.com>
2024-11-21 11:00:46 +0000

    Remove 17mov dead domain

--- src/theme/Partner/index.js
+++ src/theme/Partner/index.js
@@ -25,16 +25,6 @@ function Partners() {
             <span className="btn">10% off with ESX code</span>
           </a>
         </li>
-
-        <li className="background-box">
-          <a href="https://store.17mov.pro/">
-            <img
-              className="logo-partner"
-              src="https://esx.s3.fr-par.scw.cloud/17movement.png"
-            />
-            <span className="btn">10% off with ESX code</span>
-          </a>
-        </li>
       </ul>
     </section>
   );
